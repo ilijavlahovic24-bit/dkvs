e61a68e635431a9bf901e9a6657422e1c2d3497b
ilijavlahovic24-bit <ilija.vlahovic24@gmail.com>
2026-08-20 16:49:30 +0200

    add WAL

--- backend/lsm/wal.go
+++ backend/lsm/wal.go
@@ -1,5 +1,26 @@
 package lsm
 
-//WAL ensures durability before data reaches the MemTable.
+type Entry struct {
+	key   string
+	value []byte
+}
+
+// WAL ensures durability before data reaches the MemTable.
 type WAL struct {
+	entries []Entry
+}
+
+func (w *WAL) Append(key string, value []byte) error {
+	Entry := Entry{key: key, value: value}
+	w.entries = append(w.entries, Entry)
+	return nil
+}
+func (w *WAL) Replay() ([]Entry, error) {
+	// Implementation for replaying WAL
+	return w.entries, nil
+}
+func (w *WAL) Clear() error {
+	// Implementation for clearing WAL
+	w.entries = nil
+	return nil
 }
