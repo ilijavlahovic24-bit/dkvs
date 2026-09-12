c6e42cb22a9c862c540a2144b93ccedf942cb051
ilijavlahovic24-bit <ilija.vlahovic24@gmail.com>
2026-09-12 22:20:07 +0200

    new WAL

--- backend/lsm/wal.go
+++ backend/lsm/wal.go
@@ -1,14 +1,50 @@
 package lsm
 
+import (
+	"os"
+	"sync"
+)
+
+const (
+	opPut    byte = 0
+	opDelete byte = 1
+)
+
 type Entry struct {
-	key   string
-	value []byte
+	key     string
+	value   []byte
+	deleted bool
 }
 
 // WAL ensures durability before data reaches the MemTable.
 type WAL struct {
-	entries []Entry
+	entries     []Entry
+	path        string
+	file        *os.File
+	mu          sync.Mutex
+	syncRequest chan chan struct{}
+	done        chan struct{}
+	loopDone    chan struct{}
 }
+
+func NewWAL(path string) (*WAL, error) {
+	f, err := os.OpenFile(path, os.O_CREATE|os.O_RDWR|os.O_APPEND, 0644)
+	if err != nil {
+		return nil, err
+	}
+
+	w := &WAL{
+		path:        path,
+		file:        f,
+		syncRequest: make(chan chan struct{}, 1024),
+		done:        make(chan struct{}),
+		loopDone:    make(chan struct{}),
+	}
+	go w.syncLoop()
+	return w, nil
+}
+
+func (w *WAL) syncLoop() {}
 
 func (w *WAL) Append(key string, value []byte) error {
 	Entry := Entry{key: key, value: value}
